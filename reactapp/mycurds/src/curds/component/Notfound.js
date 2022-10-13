a2b9cfb8e59a3a9e70b3ab919dcd20beb94ae971
“[monika <[monika@webkorps.com]>
2022-10-13 14:32:53 +0530

    commit3

--- reactapp/mycurds/src/curds/component/Notfound.js
+++ reactapp/mycurds/src/curds/component/Notfound.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Notfound() {
   return (
     <div>
-      <h2>PAGE NOT FOUND</h2>
+      <h2>PAGE NOT FOUND please try again</h2>
     </div>
   )
 }
